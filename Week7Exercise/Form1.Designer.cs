e943881aeda1378b176b439d72202f48e7e5c9c0
RicDelVec <200405510@student.georgianc.on.ca>
2018-11-05 11:07:40 -0500

    first Commit

---
+++ Week7Exercise/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Week7Exercise
+{
+    partial class Form7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lblTemperature = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(330, 303);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(174, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(330, 108);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 22);
+            this.txtInput.TabIndex = 1;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(327, 227);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 17);
+            this.lblOutput.TabIndex = 2;
+            this.lblOutput.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblTemperature
+            // 
+            this.lblTemperature.AutoSize = true;
+            this.lblTemperature.Location = new System.Drawing.Point(327, 51);
+            this.lblTemperature.Name = "lblTemperature";
+            this.lblTemperature.Size = new System.Drawing.Size(192, 17);
+            this.lblTemperature.TabIndex = 3;
+            this.lblTemperature.Text = "Enter Temperature in Celsius";
+            // 
+            // Form7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTemperature);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form7";
+            this.Text = "Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lblTemperature;
+    }
+}
+
---
+++ Week7Exercise/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week7Exercise
+{
+    public partial class Form7 : Form
+    {
+        public Form7()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Input in Celsius
+            double temperature = Convert.ToDouble(txtInput.Text);
+
+            // Convert to Fahrenheit (temperature Celsius * 1.8) =32
+            double convertedTemperature = Convert.ToDouble(temperature * 1.8) + 32;
+
+            //Decision about weather
+            if(convertedTemperature >= 70)
+            {
+                lblOutput.Text = "It is " + convertedTemperature + " it's Warm!";
+                btnCalculate.BackColor = Color.Red;
+                btnCalculate.ForeColor = Color.Yellow;
+            }
+            if(convertedTemperature <70)
+            {
+                lblOutput.Text = "It is " + convertedTemperature + " it's Cool!";
+                btnCalculate.BackColor = Color.LightBlue;
+                btnCalculate.ForeColor = Color.LightGray;
+            }
+            if(convertedTemperature <= 32)
+            {
+                lblOutput.Text = "It is " + convertedTemperature + " it's Cold!";
+                btnCalculate.BackColor = Color.DarkBlue;
+                btnCalculate.ForeColor = Color.White;
+            }
+
+            //Output
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
